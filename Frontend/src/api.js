b6a7835b0566851acf6cd01fdd769cd71c898a39
roter-mond6 <tako.makhatadze6@gmail.com>
2026-06-16 18:48:44 +0400

    export api based url

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+export const API_BASE_URL = "https://kanban-application-5l4z.onrender.com";
+
 // 1. Define the api instance FIRST
 const api = axios.create({
-  baseURL: "https://kanban-application-5l4z.onrender.com", // Your actual Render URL
+  baseURL: API_BASE_URL,
 });
 
 // 2. NOW apply the interceptor to it
@@ -11,11 +13,7 @@ api.interceptors.request.use(
     const token = localStorage.getItem("token");
 
     // Checks if the request is going to your backend before adding token
-    if (
-      token &&
-      config.url &&
-      config.url.startsWith("https://kanban-application-5l4z.onrender.com")
-    ) {
+    if (token && config.url && config.url.startsWith(API_BASE_URL)) {
       config.headers.Authorization = `Bearer ${token}`;
     }
 
